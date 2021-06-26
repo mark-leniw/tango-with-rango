f90448cddb5c9fdc7ddda537b873395ec7c27b30
mark-leniw <mleniw@protonmail.com>
2021-06-26 16:04:54 -0700

    pop_script, admin, slug

--- rango/urls.py
+++ rango/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
 from django.urls.resolvers import URLPattern
 from rango import views
-from django.conf import settings
-from django.conf.urls.static import static
+
 
 app_name = 'rango'
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
