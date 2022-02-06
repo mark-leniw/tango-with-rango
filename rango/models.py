58aa197670a0c901972dc56c6b0c97840b8b74db
mark-leniw <mleniw@protonmail.com>
2022-02-06 14:46:03 -0800

    First Commit

--- rango/models.py
+++ rango/models.py
@@ -1,6 +1,8 @@
 from os import defpath
+from tkinter import CASCADE
 from django.db import models
 from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Category(models.Model):
@@ -32,3 +34,12 @@ class Page(models.Model):
 
     def __str__(self):
         return self.title
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=CASCADE)
+
+    website = models.URLField(blank=True)
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+
+    def __str__(self):
+        return self.user.username
